50e9ffce18351e2c7f345bb657709c1e12294037
Paul Vladislav <we20949@bristol.ac.uk>
2025-10-13 17:26:03 +0100

    Did some stuff in main

--- src/main.rs
+++ src/main.rs
@@ -8,7 +8,7 @@ use rand::distr::weighted;
 use csr_matrix::CSR;
 use rand::distr::weighted::WeightedIndex;
 use rand::{rng, thread_rng};
-use crate::nmpi::{calculate_npmi, get_co_counts, get_most_related_tags};
+use crate::nmpi::{get_npmi_matrix, get_co_count_matrix, get_most_related_tags};
 use crate::read_write::{write_data, read_posts, read_data};
 
 mod nmpi;
@@ -28,27 +28,23 @@ fn main() {
     // let co_counts_matrix = get_co_counts(n_posts, &tags, posts_tags_idxs).unwrap();
     // println!("\t\t\t[Done in {:?}]", start.elapsed());
     //
-
+    //
     // start = Instant::now();
     // print!("Calculating npmi matrix...");
     // let npmi_matrix = calculate_npmi(n_posts, &tags, &co_counts_matrix);
     // println!("\t\t\t[Done in {:?}]", start.elapsed());
     //
-    // println!("{:?}", npmi_matrix);
-    // println!();
-    // println!("{:?}", npmi_matrix.get_row(0).unwrap());
-    //
     // start = Instant::now();
     // print!("Writing data...");
-    // write_data("data/npmi", n_posts, &tags, &npmi_matrix);
+    // write_data("data/npmi_egs", n_posts, &tags, &npmi_matrix);
     // println!("\t\t\t\t\t[Done in {:?}]", start.elapsed());
 
     start = Instant::now();
     print!("Reading data...");
-    let (n_posts, tags, npmi_matrix) = read_data("data/npmi").unwrap();
+    let (n_posts, tags, npmi_matrix) = read_data("data/npmi_egs").unwrap();
     println!("\t\t\t\t\t[Done in {:?}]", start.elapsed());
 
-    let mut prompt_tag: Vec<&str> = vec!["loimu"];
+    let mut prompt_tag: Vec<&str> = vec!["cat"];
     for _ in 0..10 {
         let idxs = prompt_tag.iter()
             .map(|t| tags.get_idx(t).unwrap())
