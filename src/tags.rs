32e44ae4dc5c80d0f93d2f75d8bbec64d8e18442
Paul Vladislav <we20949@bristol.ac.uk>
2025-10-13 17:25:50 +0100

    Change Tags structure for performance

--- src/tags.rs
+++ src/tags.rs
@@ -4,16 +4,16 @@ use serde::{Deserialize, Deserializer, Serialize, Serializer};
 use std::collections::HashMap;
 use std::rc::Rc;
 
-#[derive(Clone, Debug, Serialize, Deserialize)]
+#[derive(Clone, Debug)]
 struct TagData {
-    idx: u32,
+    name: Rc<str>,
     count: u32,
 }
 
 #[derive(Debug, Clone)]
 pub struct Tags {
-    tag_set: FxHashMap<Rc<str>, TagData>,
-    vec: Vec<Rc<str>>,
+    tag_set: FxHashMap<Rc<str>, usize>,
+    vec: Vec<TagData>,
 }
 
 impl Tags {
@@ -24,31 +24,34 @@ impl Tags {
         }
     }
 
-    pub fn add_or_increment(&mut self, tag: &str) -> u32 {
-        if let Some(tag_data) = self.tag_set.get_mut(tag) {
-            tag_data.count += 1;
-            tag_data.idx
+    pub fn add_or_increment(&mut self, tag: &str) -> usize {
+        if let Some(idx) = self.tag_set.get(tag) {
+            self.vec[*idx].count += 1;
+            idx.to_owned()
         } else {
-            let tag: Rc<str> = Rc::from(tag);
-            self.vec.push(Rc::clone(&tag));
-            let idx = self.vec.len() as u32 - 1;
-            let count = 1;
-            self.tag_set.insert(Rc::clone(&tag), TagData { idx, count });
+            let tag_name: Rc<str> = Rc::from(tag);
+            let tag_data = TagData {
+                name: Rc::clone(&tag_name),
+                count: 1
+            };
+            self.vec.push(tag_data);
+            let idx = self.vec.len() -1;
+            self.tag_set.insert(Rc::clone(&tag_name), idx);
             idx
         }
     }
 
-    pub fn get_idx(&self, tag: &str) -> Option<u32> {
+    pub fn get_idx(&self, tag: &str) -> Option<usize> {
         if self.tag_set.contains_key(tag) {
-            Some(self.tag_set[tag].idx)
+            Some(self.tag_set.get(tag).unwrap().to_owned())
         } else {
             None
         }
     }
 
-    pub fn get_name(&self, idx: u32) -> Option<&str> {
-        if idx < self.vec.len() as u32 {
-            Some(&self.vec[idx as usize])
+    pub fn get_name(&self, idx: usize) -> Option<&str> {
+        if idx < self.vec.len() {
+            Some(&self.vec[idx].name)
         } else {
             None
         }
@@ -56,20 +59,18 @@ impl Tags {
 
     pub fn get_count(&self, name: &str) -> Option<u32> {
         if self.tag_set.contains_key(name) {
-            Some(self.tag_set[name].count)
+            let idx = self.tag_set[name];
+            Some(self.vec[idx].count)
         } else {
             None
         }
     }
 
-    pub fn get_count_idx(&self, idx: u32) -> Option<u32> {
-        let name = self.get_name(idx);
-        match name {
-            Some(name) => {
-                let count = self.get_count(name);
-                Some(count.unwrap())
-            }
-            None => None,
+    pub fn get_count_idx(&self, idx: usize) -> Option<u32> {
+        if idx < self.vec.len() {
+            Some(self.vec[idx].count)
+        } else {
+            None
         }
     }
 
@@ -78,19 +79,60 @@ impl Tags {
     }
 }
 
+impl Serialize for TagData {
+    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
+    where
+        S: Serializer,
+    {
+        let mut ser_tag_data =
+            serializer.serialize_struct("TagData", 2)?;
+
+        let name = &*self.name;
+        let count = self.count;
+
+        ser_tag_data.serialize_field("name", name)?;
+        ser_tag_data.serialize_field("count", &count)?;
+        ser_tag_data.end()
+    }
+}
+
+impl<'de> Deserialize<'de> for TagData {
+    fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
+    where
+        D: Deserializer<'de>,
+    {
+        #[derive(Deserialize)]
+        struct Helper {
+            name:  String,
+            count: u32,
+        }
+
+        let helper = Helper::deserialize(deserializer)?;
+        let name: Rc<str> = Rc::from(helper.name.as_str().to_owned());
+        let count: u32 = helper.count;
+
+        Ok(TagData { name, count })
+    }
+}
+
 impl Serialize for Tags {
     fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
     where
         S: Serializer,
     {
-        let mut ser_tags = serializer.serialize_struct("Tags", 2)?;
+        let mut ser_tags =
+            serializer.serialize_struct("Tags", 2)?;
 
-        let tag_ser_ser: FxHashMap<&str, &TagData> =
-            self.tag_set.iter().map(|(k, v)| (&**k, v)).collect();
+        let tag_set_ser: FxHashMap<&str, usize> = self.tag_set.iter()
+            .map(|(k, v)| (&**k, *v))
+            .collect();
 
-        let vec_ser: Vec<&str> = self.vec.iter().map(|v| &**v).collect();
+        let vec_ser: Vec<TagData> = self.vec.iter()
+            .map(|td| td.clone())
+            .collect();
 
-        ser_tags.serialize_field("tag_set", &tag_ser_ser)?;
+
+        ser_tags.serialize_field("tag_set", &tag_set_ser)?;
         ser_tags.serialize_field("vec", &vec_ser)?;
         ser_tags.end()
     }
@@ -103,20 +145,20 @@ impl<'de> Deserialize<'de> for Tags {
     {
         #[derive(Deserialize)]
         struct Helper {
-            tag_set: FxHashMap<String, TagData>,
-            vec: Vec<String>,
+            tag_set: FxHashMap<String, usize>,
+            vec: Vec<TagData>,
         }
 
         let helper = Helper::deserialize(deserializer)?;
-        let mut tag_set: FxHashMap<Rc<str>, TagData> = FxHashMap::default();
-        let mut vec: Vec<Rc<str>> = Vec::new();
-        for tag in helper.vec {
-            let tag_data = helper.tag_set.get(&tag).unwrap().to_owned();
-            let tag: Rc<str> = Rc::from(tag.as_str());
-            vec.push(Rc::clone(&tag));
-            tag_set.insert(Rc::clone(&tag), tag_data);
+        let mut tag_set: FxHashMap<Rc<str>, usize> = FxHashMap::default();
+        for (idx, tag) in helper.vec.iter().enumerate() {
+            let tag: Rc<str> = Rc::clone(&helper.vec[idx].name);
+            tag_set.insert(Rc::clone(&tag), idx);
         }
 
-        Ok(Tags { tag_set, vec })
+        Ok(Tags {
+            tag_set,
+            vec: helper.vec
+        })
     }
 }
